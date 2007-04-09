a7ec861720a4a598d5b6d06ae0a716f8d91172a8
Tekkub Stoutwrithe <tekkub@gmail.com>
2007-04-09 03:37:57 +0000

    Updating major versions of dongle so that it, like, you know, actually works.
    git-svn-id: http://tekkub-wow.googlecode.com/svn/trunk/FriendsWithBenefits@338 86fe6d9a-1522-0410-a387-bf9db416f0a0

--- FriendsWithBenefits.lua
+++ FriendsWithBenefits.lua
@@ -1,5 +1,5 @@
 
-FriendsWithBenefits = DongleStub("Dongle-Beta1"):New("FriendsWithBenefits")
+FriendsWithBenefits = DongleStub("Dongle-1.0-RC3"):New("FriendsWithBenefits")
 local debuggers = {Tekkub = true, Beardyhead = true, Cubbyhole = true, Mishutka = true}
 if GetRealmName() == "Area 52" and debuggers[UnitName("player")] then FriendsWithBenefits:EnableDebug(1, ChatFrame5) end
 
