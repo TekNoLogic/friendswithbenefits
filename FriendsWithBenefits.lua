466cd53910177ce60d958cfc08ddca861491aeaf
Tekkub <tekkub@gmail.com>
2009-01-09 21:57:54 -0700

    Migrate event handler away from dongle... no more dongle!

--- FriendsWithBenefits.lua
+++ FriendsWithBenefits.lua
@@ -1,7 +1,4 @@
 
-FriendsWithBenefits = DongleStub("Dongle-1.0"):New("FriendsWithBenefits")
-
-
 ----------------------
 --      Locals      --
 ----------------------
@@ -20,7 +17,18 @@ local debugf = tekDebug and tekDebug:GetFrame("FriendsWithBenefits")
 local function Debug(...) if debugf then debugf:AddMessage(string.join(", ", tostringall(...))) end end
 
 
-function FriendsWithBenefits:Initialize()
+-----------------------------
+--      Event Handler      --
+-----------------------------
+
+local f = CreateFrame("frame")
+f:SetScript("OnEvent", function(self, event, ...) if self[event] then return self[event](self, event, ...) end end)
+f:RegisterEvent("ADDON_LOADED")
+
+
+function f:ADDON_LOADED(event, addon)
+	if addon:lower() ~= "friendswithbenefits" then return end
+
 	Debug("Loading DB")
 	local factionrealm = UnitFactionGroup("player").. " - "..GetRealmName()
 
@@ -34,12 +42,21 @@ function FriendsWithBenefits:Initialize()
 	db.friends, db.removed, db.notes = db.friends or {}, db.removed or {}, db.notes or {}
 
 	if not db.removed[string.lower(UnitName("player"))] then db.friends[string.lower(UnitName("player"))] = true end
+
+	self:UnregisterEvent("ADDON_LOADED")
+	self.ADDON_LOADED = nil
+
+	if IsLoggedIn() then self:PLAYER_LOGIN() else self:RegisterEvent("PLAYER_LOGIN") end
 end
 
-function FriendsWithBenefits:Enable()
+
+function f:PLAYER_LOGIN()
 	self:RegisterEvent("CHAT_MSG_SYSTEM")
-	self:RegisterEvent("FRIENDLIST_UPDATE", "ProcessNext")
+	self:RegisterEvent("FRIENDLIST_UPDATE")
 	ShowFriends()
+
+	self:UnregisterEvent("PLAYER_LOGIN")
+	self.PLAYER_LOGIN = nil
 end
 
 
@@ -65,12 +82,12 @@ end
 -- ERR_FRIEND_WRONG_FACTION = "Friends must be part of your alliance."
 local rxadd = string.gsub(ERR_FRIEND_ADDED_S, "%%s", "(.+)")   -- ERR_FRIEND_ADDED_S = "%s added to friends."
 local rxrem = string.gsub(ERR_FRIEND_REMOVED_S, "%%s", "(.+)") -- ERR_FRIEND_REMOVED_S = "%s removed from friends list."
-function FriendsWithBenefits:CHAT_MSG_SYSTEM(event, text)
+function f:CHAT_MSG_SYSTEM(event, text)
 	if text == ERR_FRIEND_ERROR then return self:Abort("An error has occured.") end
 
 	local _, _, addname = string.find(text, rxadd)
 	local remname = not addname and select(3, string.find(text, rxrem))
-	if not addname and not remname and (not self.ProcessNext or text ~= ERR_FRIEND_NOT_FOUND and text ~= ERR_FRIEND_WRONG_FACTION) then return end
+	if not addname and not remname and (not self.FRIENDLIST_UPDATE or text ~= ERR_FRIEND_NOT_FOUND and text ~= ERR_FRIEND_WRONG_FACTION) then return end
 	if not currfriend then return end
 	Debug("Processing chat message", text, addname, remname, currfriend)
 
@@ -78,12 +95,12 @@ function FriendsWithBenefits:CHAT_MSG_SYSTEM(event, text)
 		if currop == "REM" then return self:Abort("'Not found' error when removing a friend.") end
 		db.removed[currfriend] = true
 		db.friends[currfriend] = nil
-		if self.ProcessNext then Print(string.format("Cannot find player %q on this realm.", currfriend)) end
+		if self.FRIENDLIST_UPDATE then Print(string.format("Cannot find player %q on this realm.", currfriend)) end
 	elseif text == ERR_FRIEND_WRONG_FACTION then
 		if currop == "REM" then return self:Abort("'Wrong faction' error when removing a friend.") end
 		db.removed[currfriend] = true
 		db.friends[currfriend] = nil
-		if self.ProcessNext then Print(string.format("Player %q is the wrong faction.", currfriend)) end
+		if self.FRIENDLIST_UPDATE then Print(string.format("Player %q is the wrong faction.", currfriend)) end
 	elseif addname then
 		if currop == "REM" then return self:Abort("'Friend added' message when removing a friend.") end
 		if string.lower(addname) ~= currfriend then return self:Abort("Name mismatch while adding a friend.") end
@@ -100,11 +117,11 @@ function FriendsWithBenefits:CHAT_MSG_SYSTEM(event, text)
 		friendlist[currfriend] = nil
 	end
 
-	if self.ProcessNext then self:ProcessNext() end
+	if self.FRIENDLIST_UPDATE then self:FRIENDLIST_UPDATE() end
 end
 
 
-function FriendsWithBenefits:ProcessNext(event)
+function f:FRIENDLIST_UPDATE(event)
 	if event then self:UnregisterEvent("FRIENDLIST_UPDATE") end
 
 	if initadds then
@@ -141,11 +158,15 @@ function FriendsWithBenefits:ProcessNext(event)
 	end
 
 	if hasannounced then Print("Update completed.") end
-	self.ProcessNext = nil
+	Debug("Cleaning up")
+	self:UnregisterEvent("FRIENDLIST_UPDATE")
+	self.FRIENDLIST_UPDATE, self.Cleanup = nil
 end
 
 
-function FriendsWithBenefits:Abort(msg)
+function f:Abort(msg)
 	self:UnregisterAllEvents()
+	self.FRIENDLIST_UPDATE, self.Cleanup, self.CHAT_MSG_SYSTEM, self.Abort = nil
+	self:SetScript("OnEvent", nil)
 	Print(msg, "Disabling for the rest of this session.")
 end
