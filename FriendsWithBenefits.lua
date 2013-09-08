0d0b7bf668a0b127be5766f4fb106fc33c7e01f7
tekkub :bear:  <tekkub@gmail.com>
2013-09-07 19:13:49 -0600

    Refactor to use new event handler

--- FriendsWithBenefits.lua
+++ FriendsWithBenefits.lua
@@ -2,26 +2,11 @@
 local myname, ns = ...
 
 
-----------------------
---      Locals      --
-----------------------
-
 local db, currop, currfriend, hasannounced
 local initadds, friendlist = true, {}
 
 
------------------------------
---      Event Handler      --
------------------------------
-
-local f = CreateFrame("frame")
-f:SetScript("OnEvent", function(self, event, ...) if self[event] then return self[event](self, event, ...) end end)
-f:RegisterEvent("ADDON_LOADED")
-
-
-function f:ADDON_LOADED(event, addon)
-	if addon:lower() ~= "friendswithbenefits" then return end
-
+function ns.OnLoad()
  	LibStub("tekKonfig-AboutPanel").new(nil, "FriendsWithBenefits")
 
 	ns.Debug("Loading DB")
@@ -37,21 +22,13 @@ function f:ADDON_LOADED(event, addon)
 	db.friends, db.removed, db.notes = db.friends or {}, db.removed or {}, db.notes or {}
 
 	if not db.removed[string.lower(UnitName("player"))] then db.friends[string.lower(UnitName("player"))] = true end
-
-	self:UnregisterEvent("ADDON_LOADED")
-	self.ADDON_LOADED = nil
-
-	if IsLoggedIn() then self:PLAYER_LOGIN() else self:RegisterEvent("PLAYER_LOGIN") end
 end
 
 
-function f:PLAYER_LOGIN()
-	self:RegisterEvent("CHAT_MSG_SYSTEM")
-	self:RegisterEvent("FRIENDLIST_UPDATE")
+function ns.OnLogin()
+	ns.RegisterEvent("CHAT_MSG_SYSTEM")
+	ns.RegisterEvent("FRIENDLIST_UPDATE")
 	ShowFriends()
-
-	self:UnregisterEvent("PLAYER_LOGIN")
-	self.PLAYER_LOGIN = nil
 end
 
 
@@ -86,35 +63,35 @@ end
 -- ERR_FRIEND_WRONG_FACTION = "Friends must be part of your alliance."
 local rxadd = string.gsub(ERR_FRIEND_ADDED_S, "%%s", "(.+)")   -- ERR_FRIEND_ADDED_S = "%s added to friends."
 local rxrem = string.gsub(ERR_FRIEND_REMOVED_S, "%%s", "(.+)") -- ERR_FRIEND_REMOVED_S = "%s removed from friends list."
-function f:CHAT_MSG_SYSTEM(event, text)
-	if text == ERR_FRIEND_ERROR then return self:Abort("An error has occured.") end
+function ns.CHAT_MSG_SYSTEM(event, text)
+	if text == ERR_FRIEND_ERROR then return ns.Abort("An error has occured.") end
 
 	local _, _, addname = string.find(text, rxadd)
 	local remname = not addname and select(3, string.find(text, rxrem))
-	if not addname and not remname and (not self.FRIENDLIST_UPDATE or text ~= ERR_FRIEND_NOT_FOUND and text ~= ERR_FRIEND_WRONG_FACTION) then return end
+	if not addname and not remname and (not ns.FRIENDLIST_UPDATE or text ~= ERR_FRIEND_NOT_FOUND and text ~= ERR_FRIEND_WRONG_FACTION) then return end
 	if not currfriend then return end
 	ns.Debug("Processing chat message", text, addname, remname, currfriend)
 
 	if text == ERR_FRIEND_NOT_FOUND then
-		if currop == "REM" then return self:Abort("'Not found' error when removing a friend.") end
+		if currop == "REM" then return ns.Abort("'Not found' error when removing a friend.") end
 		db.removed[currfriend] = true
 		db.friends[currfriend] = nil
-		if self.FRIENDLIST_UPDATE then ns.Printf("Cannot find player %q on this realm.", currfriend) end
+		if ns.FRIENDLIST_UPDATE then ns.Printf("Cannot find player %q on this realm.", currfriend) end
 	elseif text == ERR_FRIEND_WRONG_FACTION then
-		if currop == "REM" then return self:Abort("'Wrong faction' error when removing a friend.") end
+		if currop == "REM" then return ns.Abort("'Wrong faction' error when removing a friend.") end
 		db.removed[currfriend] = true
 		db.friends[currfriend] = nil
-		if self.FRIENDLIST_UPDATE then ns.Printf("Player %q is the wrong faction.", currfriend) end
+		if ns.FRIENDLIST_UPDATE then ns.Printf("Player %q is the wrong faction.", currfriend) end
 	elseif addname then
-		if currop == "REM" then return self:Abort("'Friend added' message when removing a friend.") end
-		if string.lower(addname) ~= currfriend then return self:Abort("Name mismatch while adding a friend.") end
+		if currop == "REM" then return ns.Abort("'Friend added' message when removing a friend.") end
+		if string.lower(addname) ~= currfriend then return ns.Abort("Name mismatch while adding a friend.") end
 		ns.Debug("Friend added", currfriend)
 		db.friends[currfriend] = true
 		db.removed[currfriend] = nil
 		friendlist[currfriend] = true
 	elseif remname then
-		if currop == "ADD" then return self:Abort("'Friend removed' message when adding a friend.") end
-		if string.lower(remname) ~= currfriend then return self:Abort("Name mismatch while removing a friend.") end
+		if currop == "ADD" then return ns.Abort("'Friend removed' message when adding a friend.") end
+		if string.lower(remname) ~= currfriend then return ns.Abort("Name mismatch while removing a friend.") end
 		ns.Debug("Friend removed", currfriend)
 		db.removed[currfriend] = true
 		db.friends[currfriend] = nil
@@ -122,12 +99,12 @@ function f:CHAT_MSG_SYSTEM(event, text)
 		db.notes[currfriend] = nil
 	end
 
-	if self.FRIENDLIST_UPDATE then self:FRIENDLIST_UPDATE() end
+	if ns.FRIENDLIST_UPDATE then ns.FRIENDLIST_UPDATE() end
 end
 
 
-function f:FRIENDLIST_UPDATE(event)
-	if event then self:UnregisterEvent("FRIENDLIST_UPDATE") end
+function ns.FRIENDLIST_UPDATE(event)
+	if event then ns.UnregisterEvent("FRIENDLIST_UPDATE") end
 
 	if initadds then
 		for i=1,GetNumFriends() do
@@ -178,14 +155,13 @@ function f:FRIENDLIST_UPDATE(event)
 
 	if hasannounced then ns.Print("Update completed.") end
 	ns.Debug("Cleaning up")
-	self:UnregisterEvent("FRIENDLIST_UPDATE")
-	self.FRIENDLIST_UPDATE, self.Cleanup = nil
+	ns.UnregisterEvent("FRIENDLIST_UPDATE")
+	ns.FRIENDLIST_UPDATE = nil
 end
 
 
-function f:Abort(msg)
-	self:UnregisterAllEvents()
-	self.FRIENDLIST_UPDATE, self.Cleanup, self.CHAT_MSG_SYSTEM, self.Abort = nil
-	self:SetScript("OnEvent", nil)
+function ns.Abort(msg)
+	ns.UnregisterAllEvents()
+	ns.FRIENDLIST_UPDATE, ns.CHAT_MSG_SYSTEM, ns.Abort = nil
 	ns.Print(msg, "Disabling for the rest of this session.")
 end
